7e0119c5ff0c9ca7fbcc047a975731dec2d324a5
Sherydan <tobar.luis.c@gmail.com>
2022-07-08 23:12:01 -0400

    changed the way of addding border to the first part. Added number validation on second part

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,38 @@
 let accordion = document.querySelector("#accordionExample");
-let btnAddBorder = document.querySelector("#add-border");
 let totalStickers = document.querySelector("#total-stickers");
 let passwordCheck = document.querySelector("#password-check");
 
-let stickerInputArray = Array.from(document.querySelectorAll("#sticker-1, #sticker-2, #sticker-3"));
-let selectArray = Array.from(document.querySelectorAll("#select-1, #select-2, #select-3"));
+let stickerInputArray = Array.from(
+  document.querySelectorAll("#sticker-1, #sticker-2, #sticker-3")
+);
+let selectArray = Array.from(
+  document.querySelectorAll("#select-1, #select-2, #select-3")
+);
 
 accordion.addEventListener("click", (e) => {
-  
-  if (e.target.id === "add-border") {
+  if (e.target.id === "honda-nsx-gt3") {
     document.querySelector("#honda-nsx-gt3").classList.toggle("add-border");
-    if (btnAddBorder.innerHTML === "Add border") {
-      btnAddBorder.innerHTML = "Remove border";
-    } else {
-      btnAddBorder.innerHTML = "Add border";
-    }
   }
-  
+
   if (e.target.id === "verify") {
-    let sum = +stickerInputArray[0].value + +stickerInputArray[1].value + +stickerInputArray[2].value;
-    if (sum <= 10) {
-      totalStickers.innerHTML = `Llevas ${sum} stickers`;
-    } else if (sum > 10){
-      totalStickers.innerHTML = "llevas demaciados stickers";
+    let sum =+stickerInputArray[0].value + +stickerInputArray[1].value + +stickerInputArray[2].value;
+
+    // check if sum variable is a valid number
+    if (!isNaN(sum)) {
+      if (sum <= 10) {
+        totalStickers.innerHTML = ` You have ${sum} stickers`;
+      } else if (sum > 10) {
+        totalStickers.innerHTML = "You have too much stickers";
+      }
+    } else {
+      totalStickers.innerHTML = "You can only enter a valid number";
     }
+
   }
 
   if (e.target.id === "submit-password") {
-    let password = selectArray[0].value + selectArray[1].value + selectArray[2].value;
+    let password =
+      selectArray[0].value + selectArray[1].value + selectArray[2].value;
     if (password === "911") {
       passwordCheck.innerHTML = "Password 1 correct";
     } else if (password === "714") {
@@ -36,5 +41,4 @@ accordion.addEventListener("click", (e) => {
       passwordCheck.innerHTML = "Password incorrect";
     }
   }
-  
 });
